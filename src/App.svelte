4cb232ca2ab0d1450e5d3dabe0f7f857db32f45b
faqihmadani <faqihfaqih22@gmail.com>
2021-05-11 14:34:52 +0700

    menjadi pencari bahan

--- src/App.svelte
+++ src/App.svelte
@@ -10,58 +10,67 @@
 		DropdownMenu,
 		DropdownToggle,
 	} from "sveltestrap";
-	import { Form, FormGroup, FormText, Input, Label } from "sveltestrap";
 
-	let menu = "Menu";
+	// let menu = "Menu";
 
-	const changeMenu = () => {
-		menu = "Menu";
-	};
+	// const changeMenu = () => {
+	// 	menu = "Menu";
+	// };
 
-	const changeIngridient = () => {
-		menu = "Ingredients";
-	};
+	// const changeIngridient = () => {
+	// 	menu = "Ingredients";
+	// };
 
 	let open = false;
-	let modalTitle = "";
-	let modalImage = "";
-	const openRecipe = (recipe) => {
+	let menu = [];
+	let menuLoading = false;
+	let menuName = "";
+	async function openRecipe(recipe) {
+		menuLoading = true;
 		open = true;
-		modalTitle = recipe.strMeal;
-		modalImage = recipe.strMealThumb;
-	};
+		const id = recipe.idMeal;
+		const response = await fetch(
+			`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+		);
+		const hasil = await response.json();
+		menu = hasil.meals;
+		menuLoading = false;
+		menuName = menu[0].strMeal;
+	}
 
 	const toggle = () => (open = !open);
 
 	let search = "";
+	let searchName = "";
 	let notFound = "";
 	let loading = false;
 	let recipes = [];
+
 	//Fetching Data
-	async function searchRecipe(e) {
+	async function searchRecipe() {
 		loading = true;
 		const response = await fetch(
-			`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+			`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`
 		);
 		const hasil = await response.json();
 		recipes = hasil.meals;
 		loading = false;
+		searchName = search;
 		if (!recipes) {
 			notFound = search;
 		}
+		search = "";
 	}
 </script>
 
 <main>
 	<h1 class="text-primary">Recipe App</h1>
-	<div class="search-input">
-		<FormGroup>
-			<Input type="text" placeholder="Search recipe" bind:value={search} />
-		</FormGroup>
-		<Button color="primary" on:click={searchRecipe}>Search</Button>
-	</div>
+	<form on:submit|preventDefault={searchRecipe} class="search-input" action="">
+		<input type="text" placeholder="Search recipe" bind:value={search} />
+		<Button color="primary" type="submit">Search</Button>
+	</form>
 
-	<div class="dropdown">
+	<!-- <div class="dropdown">
 		<h4 class="mx-2">Search by :</h4>
 		<Dropdown>
 			<DropdownToggle color="primary" outline caret>{menu}</DropdownToggle>
@@ -74,17 +83,20 @@
 				>
 			</DropdownMenu>
 		</Dropdown>
-	</div>
+	</div> -->
 
 	{#if loading}
-		<Spinner color="primary" />
+		<Spinner class="my-5" color="primary" />
 	{/if}
 	{#if !loading}
 		<div>
 			{#if recipes}
+				{#if searchName}
+					<h5 class="my-5">Displays foods made from "{searchName}"</h5>
+				{/if}
 				<Row class="my-5">
 					{#each recipes as recipe (recipe.idMeal)}
-						<Col xs="4" class="my-3">
+						<Col md="4" class="my-3">
 							<div class="pointer" on:click={() => openRecipe(recipe)}>
 								<Card body>
 									<img
@@ -100,19 +112,33 @@
 				</Row>
 			{/if}
 			{#if !recipes}
-				<h3>{notFound} is not found.</h3>
+				<h5 class="my-5">
+					Food made from "{notFound}" was not found.
+				</h5>
 			{/if}
 		</div>
 	{/if}
 	<div>
-		<Modal body header={modalTitle} isOpen={open} {toggle}>
-			<img
-				src={modalImage}
-				alt={modalTitle}
-				class="img-thumbnail rounded mx-auto d-block my-2"
-			/>
-			<p class="d-block text-center font-weight-bold mt-5">Ingredients :</p>
-			<p class="ingredients text-center">oi</p>
+		<Modal body header={menuName} isOpen={open} {toggle}>
+			{#if menuLoading}
+				<div class="center">
+					<Spinner class="my-5 mx-auto" color="primary" />
+				</div>
+			{/if}
+			{#if !menuLoading}
+				<img
+					src={menu[0].strMealThumb}
+					alt={menu[0].strMeal}
+					class="img-thumbnail rounded mx-auto d-block my-2"
+				/>
+				<p class="d-block text-center font-weight-bold">Category :</p>
+				<p class="ingredients text-center">{menu[0].strCategory}</p>
+				<p class="d-block text-center font-weight-bold">Instructions :</p>
+				<p class="ingredients">{menu[0].strInstructions}</p>
+				<a href={menu[0].strYoutube} class="youtube" target="_blank"
+					>Watch Video</a
+				>
+			{/if}
 		</Modal>
 	</div>
 </main>
@@ -124,17 +150,41 @@
 		max-width: 240px;
 		margin: 0 auto;
 	}
+	.youtube {
+		margin-top: 40px;
+		display: block;
+		text-align: center;
+	}
+	.center {
+		display: flex;
+		width: 100%;
+		height: 100%;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.search-input {
+		margin-top: 30px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	input {
+		width: 400px;
+		margin-right: 10px;
+	}
 
 	.pointer {
 		cursor: pointer;
 	}
 
-	.dropdown {
+	/* .dropdown {
 		margin: 50px;
 		display: flex;
 		justify-content: center;
 		align-items: center;
-	}
+	} */
 
 	h1 {
 		color: #ff3e00;
