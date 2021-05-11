ceb584a84944c7e4750a8c55c702249e591c3d17
faqihmadani <faqihfaqih22@gmail.com>
2021-05-11 21:00:33 +0700

    menambahkan fitur mencari nama menu

--- src/App.svelte
+++ src/App.svelte
@@ -11,32 +11,41 @@
 		DropdownToggle,
 	} from "sveltestrap";
 
-	// let menu = "Menu";
+	let mode = "Menu";
 
-	// const changeMenu = () => {
-	// 	menu = "Menu";
-	// };
+	const changeMenu = () => {
+		mode = "Menu";
+	};
 
-	// const changeIngridient = () => {
-	// 	menu = "Ingredients";
-	// };
+	const changeIngredient = () => {
+		mode = "Ingredient";
+	};
 
 	let open = false;
 	let menu = [];
 	let menuLoading = false;
 	let menuName = "";
 	async function openRecipe(recipe) {
-		menuName = "";
-		menuLoading = true;
-		open = true;
-		const id = recipe.idMeal;
-		const response = await fetch(
-			`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-		);
-		const hasil = await response.json();
-		menu = hasil.meals;
-		menuLoading = false;
-		menuName = menu[0].strMeal;
+		if (mode === "Ingredient") {
+			menuName = "";
+			menuLoading = true;
+			open = true;
+			const id = recipe.idMeal;
+			const response = await fetch(
+				`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+			);
+			const hasil = await response.json();
+			menu = hasil.meals;
+			menuLoading = false;
+			menuName = menu[0].strMeal;
+		} else if (mode === "Menu") {
+			menuName = "";
+			menuLoading = true;
+			open = true;
+			menu = recipe;
+			menuLoading = false;
+			menuName = menu.strMeal;
+		}
 	}
 
 	const toggle = () => (open = !open);
@@ -49,46 +58,71 @@
 
 	//Fetching Data
 	async function searchRecipe() {
-		if (search === "") {
-			alert("Please input the ingredients of the food that you want to find.");
-		} else {
-			loading = true;
-			const response = await fetch(
-				`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`
-			);
-			const hasil = await response.json();
-			recipes = hasil.meals;
-			loading = false;
-			searchName = search;
-			if (!recipes) {
-				notFound = search;
+		if (mode === "Ingredient") {
+			if (search === "") {
+				alert(
+					"Please input the ingredients of the food that you want to find."
+				);
+			} else {
+				loading = true;
+				const response = await fetch(
+					`https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`
+				);
+				const hasil = await response.json();
+				recipes = hasil.meals;
+				loading = false;
+				searchName = search;
+				if (!recipes) {
+					notFound = search;
+				}
+				search = "";
 			}
-			search = "";
+		} else if (mode === "Menu") {
+			if (search === "") {
+				alert("Please input the name of the food that you want to find.");
+			} else {
+				loading = true;
+				const response = await fetch(
+					`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+				);
+				const hasil = await response.json();
+				recipes = hasil.meals;
+				loading = false;
+				searchName = search;
+				if (!recipes) {
+					notFound = search;
+				}
+				search = "";
+			}
 		}
 	}
 </script>
 
 <main>
-	<h2 class="text-primary mb-5">Find Meals for Your Ingredients</h2>
+	<h2 class="text-primary mb-5">Find Your Meals</h2>
 	<form on:submit|preventDefault={searchRecipe} class="search-input" action="">
-		<input type="text" placeholder="Write the ingredient" bind:value={search} />
+		<input
+			type="text"
+			placeholder="Write the {mode.toLowerCase()} ..."
+			bind:value={search}
+		/>
 		<Button color="primary" type="submit">Search</Button>
 	</form>
 
-	<!-- <div class="dropdown">
-		<h4 class="mx-2">Search by :</h4>
+	<div class="dropdown">
+		<h5 class="mx-2">Search by :</h5>
 		<Dropdown>
-			<DropdownToggle color="primary" outline caret>{menu}</DropdownToggle>
+			<DropdownToggle color="primary" outline caret>{mode}</DropdownToggle>
 			<DropdownMenu>
 				<DropdownItem header class="btn text-left" on:click={changeMenu}
 					>Menu</DropdownItem
 				>
-				<DropdownItem header class="btn text-left" on:click={changeIngridient}
-					>Ingrdients</DropdownItem
+				<DropdownItem header class="btn text-left" on:click={changeIngredient}
+					>Ingredient</DropdownItem
 				>
 			</DropdownMenu>
 		</Dropdown>
-	</div> -->
+	</div>
 
 	{#if loading}
 		<Spinner class="my-5" color="primary" />
@@ -96,8 +130,11 @@
 	{#if !loading}
 		<div>
 			{#if recipes}
-				{#if searchName}
+				{#if searchName && mode === "Ingredient"}
 					<h5 class="my-5">Displays foods made from "{searchName}"</h5>
+				{/if}
+				{#if searchName && mode === "Menu"}
+					<h5 class="my-5">Displays foods named "{searchName}"</h5>
 				{/if}
 				<Row class="my-5">
 					{#each recipes as recipe (recipe.idMeal)}
@@ -109,16 +146,21 @@
 										class="card-img-top mb-3"
 										alt={recipe.strCategory}
 									/>
-									<h5 class="card-title">{recipe.strMeal}</h5>
+									<h6 class="card-title">{recipe.strMeal}</h6>
 								</Card>
 							</div>
 						</Col>
 					{/each}
 				</Row>
 			{/if}
-			{#if !recipes}
+			{#if !recipes && mode === "Ingredient"}
 				<h5 class="my-5">
 					Food made from "{notFound}" was not found.
+				</h5>
+			{/if}
+			{#if !recipes && mode === "Menu"}
+				<h5 class="my-5">
+					Food named "{notFound}" was not found.
 				</h5>
 			{/if}
 		</div>
@@ -130,7 +172,7 @@
 					<Spinner class="my-5 mx-auto" color="primary" />
 				</div>
 			{/if}
-			{#if !menuLoading}
+			{#if !menuLoading && mode === "Ingredient"}
 				<img
 					src={menu[0].strMealThumb}
 					alt={menu[0].strMeal}
@@ -144,6 +186,19 @@
 					>Watch Video</a
 				>
 			{/if}
+			{#if !menuLoading && mode === "Menu"}
+				<img
+					src={menu.strMealThumb}
+					alt={menu.strMeal}
+					class="img-thumbnail rounded mx-auto d-block my-2"
+				/>
+				<p class="d-block text-center font-weight-bold">Category :</p>
+				<p class="ingredients text-center">{menu.strCategory}</p>
+				<p class="d-block text-center font-weight-bold">Instructions :</p>
+				<p class="ingredients">{menu.strInstructions}</p>
+				<a href={menu.strYoutube} class="youtube" target="_blank">Watch Video</a
+				>
+			{/if}
 		</Modal>
 	</div>
 </main>
@@ -154,6 +209,13 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+	}
+
+	.dropdown {
+		margin-top: 55px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 	.youtube {
 		margin-top: 40px;
